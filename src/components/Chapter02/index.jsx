999abeaa5f58b1126ef9d4869be037e7c13ad2be
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-20 09:05:22 +0200

    feat: create simple components (Home, Chapter02, Chapter03)

---
+++ src/components/Chapter02/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Chapter02 = () => {
+  return <h2>Chapter - 2</h2>;
+};
+
+export default Chapter02;
---
+++ src/components/Chapter03/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Chapter03 = () => {
+  return <h2>Chapter - 3</h2>;
+};
+
+export default Chapter03;
