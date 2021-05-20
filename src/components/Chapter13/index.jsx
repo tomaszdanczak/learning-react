f00fc11265f25595daa497834034995a07711914
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-20 10:01:09 +0200

    feat: add chapter 13

--- src/components/Chapter13/index.jsx
+++ src/components/Chapter13/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import HelloWorld from "./HelloWorld";
+import "./index.css";
 
 const Chapter13 = () => {
-  return <h2>Chapter - 13</h2>;
+  return (
+    <div>
+      <h1>Chapter 13</h1>
+      <div className="body--chapter13">
+        <HelloWorld />
+      </div>
+    </div>
+  );
 };
 
 export default Chapter13;
