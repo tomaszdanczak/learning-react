39e2ed38ef9f963badcfcaae0988dc811e86e0fc
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-20 10:09:23 +0200

    feat: add chapter 15

--- src/components/Chapter15/index.jsx
+++ src/components/Chapter15/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./index.css";
+import TodoList from "./TodoList";
 
 const Chapter15 = () => {
-  return <h2>Chapter - 15</h2>;
+  return (
+    <div>
+      <h1>Chapter - 15</h1>
+      <div className="body--chapter15">
+        <div className="wrapper">
+          <TodoList />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chapter15;
