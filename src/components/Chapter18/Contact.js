0bdffb5a65838d8a10cdd87c3a427e77b18b5dd7
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-20 10:18:50 +0200

    feat: add chapter 18

---
+++ src/components/Chapter18/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Maszy pytania?</h2>
+        <p>
+          Najlepiej wyślij wiadomość przez nasze{" "}
+          <a href="http://forum.kirupa.com">forum</a>.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
--- src/components/Chapter18/index.jsx
+++ src/components/Chapter18/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Main from "./Main";
 
 const Chapter18 = () => {
-  return <h2>Chapter - 18</h2>;
+  return (
+    <div>
+      <h1>Chapter - 18</h1>
+      <div className="body--chapter18">
+        <Main />
+      </div>
+    </div>
+  );
 };
 
 export default Chapter18;
