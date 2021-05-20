1fb4ea845b2462761ed38fe5f31cb4e37948dbb9
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-20 09:53:05 +0200

    feat: add chapter 06

--- src/components/Chapter06/index.jsx
+++ src/components/Chapter06/index.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
+class Display extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.color}</p>
+        <p>{this.props.num}</p>
+        <p>{this.props.size}</p>
+      </div>
+    );
+  }
+}
+
+class Label extends React.Component {
+  render() {
+    return <Display {...this.props} />;
+  }
+}
+
+class Shirt extends React.Component {
+  render() {
+    return (
+      <div>
+        <Label {...this.props} />
+      </div>
+    );
+  }
+}
+
 const Chapter06 = () => {
-  return <h2>Chapter - 06</h2>;
+  return (
+    <div>
+      <h1>Chapter 6</h1>
+      <Shirt color="jasnoniebieski" num="3.14" size="średni" />
+    </div>
+  );
 };
 
 export default Chapter06;
