575bf2a962eb16bb4957b977000e666ed9009c77
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-20 09:09:33 +0200

    feat: install react-router-dom and create Switch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Navigation from "./components/Navigation";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Home from "./components/Home";
+import Chapter02 from "./components/Chapter02";
+import Chapter03 from "./components/Chapter03";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <div className="content">This is a content</div>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <div className="content">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/chapter02" component={Chapter02} />
+            <Route path="/chapter03" component={Chapter03} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
