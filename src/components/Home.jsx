aca8993dabc738b32bfd733c557d7665fcd28419
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-20 09:19:52 +0200

    feat: add Jumbotron in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Jumbotron, Container, Button } from "react-bootstrap";
 
 const Home = () => {
-  return <h2>Home</h2>;
+  return (
+    <Jumbotron>
+      <Container>
+        <h1>Learning React</h1>
+        <p>
+          This is my work with book Learning React by{" "}
+          <a href="https://www.kirupa.com/react/">Kirupa Chinnathambi</a>
+        </p>
+        <Button
+          href="https://helion.pl/ksiazki/react-i-redux-praktyczne-tworzenie-aplikacji-www-wydanie-ii-kirupa-chinnathambi,rerew2.htm#format/d"
+          variant="primary"
+        >
+          See book
+        </Button>
+      </Container>
+    </Jumbotron>
+  );
 };
 
 export default Home;
