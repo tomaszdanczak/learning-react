da6e46f78defb7672f1aad44bd3952f0c02bfc15
Tomasz Dańczak <tomaszdanczak@gmail.com>
2021-05-20 08:58:36 +0200

    feat: add react-bootstrap and create simple structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar fixed="top" expand="sm">
+        <Navbar.Brand>
+          <img alt="Logo" src={logo} /> Logo
+        </Navbar.Brand>
+
+        <Navbar.Toggle />
+
+        <Navbar.Collapse>
+          <Nav>
+            <NavDropdown title="Products">
+              <NavDropdown.Item href="blog">Blog</NavDropdown.Item>
+              <NavDropdown.Item href="about-us">About Us</NavDropdown.Item>
+              <NavDropdown.Item href="contact-us">Contact Us</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="content">This is a content</div>
     </div>
   );
 }
